a170d38c52c461b9a573253837ebf2d53288fedb
mansii1 <mansimangukiya0@gmail.com>
2025-08-11 23:19:52 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [netlify()],
+  server: {
+    port: 3000,
+  },
+});
