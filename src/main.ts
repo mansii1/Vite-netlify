91d8872be8bc1e60611e8e33068c009338ddbf01
mansii1 <mansimangukiya0@gmail.com>
2025-08-13 23:14:36 +0530

    First slide completed

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const panels = document.querySelectorAll(".panel");
+console.log(panels, "panel");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
